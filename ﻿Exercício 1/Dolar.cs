07654b777e004b7bf078a48d0cf8eb7d642a0a45
Caique Pereira Alves <caiquewin@gmail.com>
2020-09-11 15:16:35 -0300

    Projeto criado e finalisado

---
+++ ﻿Exercício 1/Dolar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace _Exercício_1
+{
+    class Dolar
+    {
+        private Double ValorDolar{ get { return 5.36; } }
+        public Double Quantidade { get; set; }
+
+        public Dolar(double quantidade)
+        {
+           Quantidade = quantidade;
+        }
+
+        public double TotalSaida()
+        {
+            return ValorDolar * Quantidade;
+        }
+        public override string ToString()
+        {
+            return "Saida: "+TotalSaida().ToString("f2"); 
+        }
+    }
+}
---
+++ ﻿Exercício 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace _Exercício_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			
+			try
+			{
+				Console.Write("Entrada: ");
+				double entrada = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+				Dolar dolar = new Dolar(entrada);
+				Console.WriteLine(dolar);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"Erro: {e}");
+			}
+        }
+    }
+}
